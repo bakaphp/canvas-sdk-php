be5c9387ed74669185589cad3d00f8633df98362
Rafael White <rwhite@mctekk.com>
2019-09-19 17:26:44 -0400

    Add request function to guzzle client file

--- src/HttpClient/GuzzleClient.php
+++ src/HttpClient/GuzzleClient.php
@@ -9,4 +9,21 @@ use Canvas\Util;
 
 class GuzzleClient implements ClientInterface
 {
+    /**
+     * @param string $method The HTTP method being used
+     * @param string $absoluteUrl The URL being requested, including domain and protocol
+     * @param array $headers Headers to be used in the request (full strings, not Key-Value pairs)
+     * @param array $params Key-Value pairs for parameters. Can be nested for arrays and hashes
+     * @param boolean $hasFile Whether or not $params references a file (via an @ prefix or
+     *                         CurlFile)
+     *
+     * @throws \Canvas\Exception\Api
+     * @throws \Canvas\Exception\ApiConnection
+     * @return array An array whose first element is raw request body, second
+     *    element is HTTP status code and third array of HTTP headers.
+     */
+    public function request($method, $absoluteUrl, $headers, $params, $hasFile)
+    {
+
+    }
 }
