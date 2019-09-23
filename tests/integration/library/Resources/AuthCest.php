679dd00ace1ca599ea79cf9d6b2798c30f15ea37
Rafael White <rwhite@mctekk.com>
2019-09-23 16:06:51 -0400

    Add signup test on authcest

--- tests/integration/library/Resources/AuthCest.php
+++ tests/integration/library/Resources/AuthCest.php
@@ -4,6 +4,7 @@ namespace Gewaer\Tests\Integration\library\Resources;
 
 use IntegrationTester;
 use Canvas\Resources\Auth;
+use Canvas\Resources\Users;
 use Canvas\Canvas;
 use Phalcon\Security\Random;
 
@@ -25,6 +26,10 @@ class AuthCest
     public function onConstruct(): void
     {
         $this->random =  new Random();
+        Canvas::setApiKey($this->random->base58());
+        Auth::auth(['email'=> 'max@mctekk.com','password'=>'nosenose']);
+        $this->userEmail = 'example-'. $this->random->base58() .'@gmail.com';
+        $this->userPassword = $this->random->base58();
 
     }
 
@@ -64,5 +69,27 @@ class AuthCest
         Canvas::setApiKey($this->random->base58());
         $auth = Auth::auth(['email'=> 'nobody@baka.io','password'=>'bakatest123567']);
         $I->assertTrue($auth->id == self::DEFAULT_CANVAS_USER_ID);
+    }
+
+    /**
+     * Create a new user
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function signup(IntegrationTester $I): void
+    {
+        $users = Users::create([
+            'firstname'=>'testSDK',
+            'lastname'=> 'testSDK',
+            'displayname'=> 'sdktester',
+            'password'=> $this->userPassword,
+            'default_company'=> 'Company'. $this->random->base58(),
+            'email'=> $this->userEmail,
+            'verify_password'=> $this->userPassword
+        ]);
+
+        $I->assertTrue(gettype($users) == 'object');
+        $this->userId = $users->user->id;
     }
 }
