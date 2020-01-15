87240a667cceb8ca13d9ec42dfe3d26470e00d02
Rafael White <rwhite@mctekk.com>
2020-01-15 11:10:41 -0400

    Add Users and BaseModel

---
+++ src/Models/Users.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Canvas\Models;
+
+use Canvas\Users as UserResource;
+
+/**
+ * Users Class.
+ */
+class Users
+{
+    /**
+     * Overwrite the user create function to return a usr object like we expect.
+     *
+     * @param array|null $params
+     * @param array|string|null $options
+     *
+     * @return object stdClass
+     */
+    public static function find($params = null, $opts = null)
+    {
+        $searchBy = [];
+        if ($params['conditions']) {
+            // Find for OR or AND statements and push them to an array
+            // $conditions = str_replace('and',',', $params['conditions']);
+            $conditions = preg_split("/\b(?:and|or)\b/", $params['conditions']);
+
+            //If there is a bind among the params then we need to map the conditions wildcards to the elements on bind
+            foreach ($conditions as $key => $value) {
+                if (isset($params['bind'])) {
+                    $bindValue = $params['bind'][$key];
+                    $conditions[$key] = !is_numeric($bindValue) ? str_replace('= ?' . $key, ':%' . $bindValue . '%', $value) : str_replace('= ?' . $key, ':' . $bindValue, $value);
+                } else {
+                    $conditionArray = explode(' ', rtrim($value));
+                    $conditions[$key] = !is_numeric(end($conditionArray)) ? str_replace('= '. end($conditionArray), ':%' . end($conditionArray) . '%', $value) : str_replace('= '. end($conditionArray), ':' . end($conditionArray), $value);
+                }
+            }
+            $searchBy['conditions'] = $conditions;
+        }
+
+        if ($params['order']) {
+            $params['order'] = strpos($params['order'], 'DESC') ? str_replace(' DESC', '|desc', $params['order']) : str_replace(' ASC', '|ASC', $params['order']);
+            $searchBy['order'] = $params['order'];
+        }
+
+        return $searchBy;
+
+        return UserResource::all([], [
+            'conditions' => ['firstname:%Max%', 'is_deleted:0'],
+            'sort' => 'firstname|desc'
+        ]);
+    }
+
+    /**
+     * Overwrite the user create function to return a usr object like we expect.
+     *
+     * @param array|null $params
+     * @param array|string|null $options
+     *
+     * @return object stdClass
+     */
+    public static function findFirst($params = null, $opts = null)
+    {
+        return Users::retrieve('2', [], ['relationships' => ['roles']]);
+    }
+}
