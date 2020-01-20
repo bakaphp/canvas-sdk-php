6fa19dcc9542d0065d127371323aa5b1bd0a7b82
Rafael White <rwhite@mctekk.com>
2020-01-20 10:49:14 -0400

    Remove variables for private and public routes

--- src/Routes/PublicRoutes.php
+++ src/Routes/PublicRoutes.php
@@ -5,31 +5,29 @@ namespace Kanvas\Sdk\Routes;
 use Baka\Router\Route;
 
 /**
- * Default Kanvas Public Routes Class
+ * Default Kanvas Public Routes Class.
  */
 class PublicRoutes
 {
-    private $publicRoutes = [
-        Route::get('/')->controller('ApiController')->action('transporter'),
-        Route::post('/auth')->controller('ApiController')->action('transporter'),
-        Route::post('/refresh-token')->controller('ApiController')->action('transporter'),
-        Route::post('/users')->controller('ApiController')->action('transporter'),
-        Route::post('/auth/forgot')->controller('ApiController')->action('transporter'),
-        Route::post('/auth/reset/{key}')->controller('ApiController')->action('transporter'),
-        Route::get('/users-invite/validate/{hash}')->controller('ApiController')->action('transporter'),
-        Route::post('/users-invite/{hash}')->controller('ApiController')->action('transporter'),
-        Route::post('/webhook/payments')->controller('ApiController')->action('transporter'),
-        Route::get('/apps/{key}/settings')->controller('ApiController')->action('transporter'),
-        Route::post('/users/social')->controller('ApiController')->action('transporter')
-    ];
-
     /**
-     * Get all Public Routes
+     * Get all Public Routes.
      *
      * @return array
      */
     public static function getRoutes(): array
     {
-        return self::$publicRoutes;
+        return [
+            Route::get('/')->controller('ApiController')->action('transporter'),
+            Route::post('/auth')->controller('ApiController')->action('transporter'),
+            Route::post('/refresh-token')->controller('ApiController')->action('transporter'),
+            Route::post('/users')->controller('ApiController')->action('transporter'),
+            Route::post('/auth/forgot')->controller('ApiController')->action('transporter'),
+            Route::post('/auth/reset/{key}')->controller('ApiController')->action('transporter'),
+            Route::get('/users-invite/validate/{hash}')->controller('ApiController')->action('transporter'),
+            Route::post('/users-invite/{hash}')->controller('ApiController')->action('transporter'),
+            Route::post('/webhook/payments')->controller('ApiController')->action('transporter'),
+            Route::get('/apps/{key}/settings')->controller('ApiController')->action('transporter'),
+            Route::post('/users/social')->controller('ApiController')->action('transporter')
+        ];
     }
 }
