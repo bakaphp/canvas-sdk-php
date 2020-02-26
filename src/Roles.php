dc09a9cf25b29157eddb79f4bf1e5006cd3738e8
Rafael White <rwhite@mctekk.com>
2020-02-26 11:48:08 -0400

    Add static functon on Apps and Roles resources for Permissions Trait. Add new UserRole Trait

--- src/Roles.php
+++ src/Roles.php
@@ -13,20 +13,22 @@ use Kanvas\Sdk\Api\Resource;
 use Kanvas\Sdk\Util\Util;
 use Kanvas\Sdk\Users;
 use Kanvas\Sdk\Apps;
+use Exception;
 
 /**
- * Filesystem Resource
+ * Filesystem Resource.
  */
 class Roles extends Resource
 {
     const OBJECT_NAME = 'roles';
+    const DEFAULT_ACL_APP_ID = 1;
+    const DEFAULT_ACL_COMPANY_ID = 1;
 
     use All;
     use Retrieve;
 
-
     /**
-     * Get App Id by its key
+     * Get App Id by its key.
      *
      * @param string $key
      * @return object
@@ -34,6 +36,46 @@ class Roles extends Resource
     public static function getUserRole(int $appId)
     {
         $currentCompanyId = Users::getSelf()->default_company;
-        return current(self::all([], ['conditions'=> ["companies_id:{$currentCompanyId}","apps_id:{$appId}","is_deleted:0"]]));
+        return current(self::all([], ['conditions' => ["companies_id:{$currentCompanyId}", "apps_id:{$appId}", 'is_deleted:0']]));
+    }
+
+    /**
+     * Get Role by name.
+     *
+     * @param string $role
+     * @param int $currentCompanyId
+     * @param string $appKey
+     * @return object
+     */
+    public static function getByAppName(string $role, int $currentCompanyId, string $appKey)
+    {   
+        if (strpos($role, '.') === false) {
+            throw new Exception('ACL - We are expecting the app for this role');
+        }
+
+        $appRole = explode('.', $role);
+        $role = $appRole[1];
+        $appName = $appRole[0];
+
+        //look for the app and set it
+        if (!$app = Apps::getACLApp($appName, $appKey)) {
+            throw new Exception('ACL - No app found for this role');
+        }
+
+        $currentAppRole = current(self::all([], [
+            'conditions' => [
+                "companies_id:{$currentCompanyId}", 
+                "apps_id:{$app->id}"
+        ]]));
+
+        if ($currentAppRole instanceof KanvasObject) {
+            return $currentAppRole;
+        }
+
+        // return current(self::all([], [
+        //     'conditions' => [
+        //         "companies_id:" . self::DEFAULT_ACL_COMPANY_ID, 
+        //         "apps_id:" . self::DEFAULT_ACL_APP_ID
+        // ]]));
     }
 }
