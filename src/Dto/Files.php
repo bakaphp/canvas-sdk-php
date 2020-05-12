f36fd7c8417623921f9b569d985cc070a844c2b6
Rafael White <rwhite@mctekk.com>
2020-05-12 11:35:59 -0400

    Add Files mapper and dto. Also add service on FilesystemEntitiesTrait

---
+++ src/Dto/Files.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Kanvas\Sdk\Dto;
+
+class Files
+{
+    /**
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     *
+     * @var int
+     */
+    public $filesystem_id;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $field_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $url;
+
+    /**
+     *
+     * @var string
+     */
+    public $size;
+
+    /**
+     *
+     * @var string
+     */
+    public $file_type;
+
+    /**
+     *
+     * @var array
+     */
+    public $attributes;
+}
