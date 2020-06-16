099de695ad7fe811db122fb594470c3c3d689225
Rafael White <rwhite@mctekk.com>
2020-06-16 11:56:06 -0400

    Remove unused functions from passthrough trait

--- src/Passthroughs/PhalconPassthrough.php
+++ src/Passthroughs/PhalconPassthrough.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Kanvas\Sdk\Passthroughs;
 
 use CURLFile;
-use GuzzleHttp\Client;
 use Kanvas\Sdk\Kanvas;
 use Kanvas\Sdk\Resources;
 use Phalcon\Http\Response;
@@ -38,21 +37,13 @@ trait PhalconPassthrough
      */
     public function onConstruct()
     {
-        $this->setApiHeaders([
-            'Authorization' => $this->request->hasHeader('Authorization') ? $this->request->getHeader('Authorization') : '',
-        ]);
-    }
+        if ($this->request->hasHeader('Authorization')) {
+            Resources::getClient()->setAuthToken($this->request->getHeader('Authorization'));
+        }
 
-    /**
-     * Function to set the API headers since the property is protected.
-     *
-     * @param mixed $version
-     *
-     * @return void
-     */
-    public function setApiHeaders($apiHeaders) : void
-    {
-        $this->apiHeaders = $apiHeaders;
+        if ($this->request->hasFiles()) {
+            $this->apiHeaders['Content-Type'] = 'multipart/form-data';
+        }
     }
 
     /**
@@ -77,59 +68,12 @@ trait PhalconPassthrough
      */
     public function transporter() : Response
     {
-        $headers = [];
-        // Get all router params
-        $routeParams = $this->router->getParams();
-
         $uri = str_replace('/v1/', '', $this->router->getRewriteUri());
         $method = $this->request->getMethod();
 
-        if ($this->request->hasHeader('Authorization')) {
-            Resources::getClient()->setAuthToken($this->request->getHeader('Authorization'));
-        }
-
-        if ($this->request->hasFiles()) {
-            $headers['Content-Type'] = 'multipart/form-data';
-        }
-
-        $response = Resources::getClient()->call($method, $uri, $headers, $this->getData());
+        $response = Resources::getClient()->call($method, $uri, $this->apiHeaders, $this->getData());
 
         return $this->response($response);
-    }
-
-    /**
-     * Function that executes the request to the configured API.
-     *
-     * @param string $method - The request method
-     * @param string $url - The request URL
-     * @param array $data - The form data
-     *
-     * @return JSON
-     */
-    public function makeRequest($url, $method = 'GET', $data = [])
-    {
-        $client = new Client();
-
-        $parse = function ($error) {
-            if ($error->hasResponse()) {
-                return $error->getResponse();
-            }
-
-            return json_decode($error->getMessage());
-        };
-
-        try {
-            $response = $client->request($method, $url, $this->getRequestData($method, $data));
-            return $response;
-        } catch (\GuzzleHttp\Exception\BadResponseException $error) {
-            return $parse($error);
-        } catch (\GuzzleHttp\Exception\ClientException $error) {
-            return $parse($error);
-        } catch (\GuzzleHttp\Exception\ConnectException $error) {
-            return $parse($error);
-        } catch (\GuzzleHttp\Exception\RequestException $error) {
-            return $parse($error);
-        }
     }
 
     /**
@@ -189,19 +133,5 @@ trait PhalconPassthrough
         }
 
         return $files;
-    }
-
-    /**
-     * Get the record by its primary key.
-     *
-     * @param mixed $id
-     *
-     * @throws Exception
-     *
-     * @return Response
-     */
-    public function getById($id) : Response
-    {
-        return $this->transporter();
     }
 }
